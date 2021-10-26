da1fe4fe16d69109dd7354e007588a6fb3c84545
Jarosław Bielski <bielski.j@gmail.com>
2021-10-26 22:35:44 +0200

    Added basic unit-test sample. Added empty uCoroutine implementation

---
+++ src/uCoroutine.c
@@ -0,0 +1,34 @@
+/*
+ * uCoroutine.c
+ *
+ *  Created on: 26.10.2021
+ *      Author: Jaroslaw Bielski (bielski.j@gmail.com)
+ */
+
+
+#include "uCoroutine.h"
+
+
+void uCoroutine_initialize(void) {
+
+}
+
+
+void uCoroutine_terminate(void) {
+
+}
+
+#ifdef UCOROUTINE_CONFIG_DYNAMIC_ALLOCATION
+uCoroutinePtr uCoroutine_new() {
+
+}
+#endif
+
+void uCoroutine_schedule(void) {
+
+}
+
+
+void uCoroutine_interrupt(void) {
+
+}
---
+++ test/static_api/src/test.cpp
@@ -0,0 +1,8 @@
+/*
+ * test.cpp
+ *
+ *  Created on: 26.10.2021
+ *      Author: Jaroslaw Bielski (bielski.j@gmail.com)
+ */
+
+
