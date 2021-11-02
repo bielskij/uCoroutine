493b221910d79de55bc65b123b1fbfba5af226b2
Jarosław Bielski <bielski.j@gmail.com>
2021-11-02 23:21:24 +0100

    Added more API and some docs

--- include/uCoroutine.h
+++ include/uCoroutine.h
@@ -17,12 +17,42 @@
 #include "uCoroutine/internal.h"
 #include "uCoroutine/config.h"
 
+/*!
+ * The following two macros define a coroutine block of code. They need a pointer to uCoroutine
+ * structure.
+ */
+#define UCOROUTINE_BEGIN(_ucPtr) __UCOROUTINE_BEGIN(_ucPtr)
+#define UCOROUTINE_END()         __UCOROUTINE_END()
 
+/*!
+ * Useful macros for simple defining coroutine functions.
+ *
+ * @param __name     coroutine function name
+ * @param __dataType type of user data. User data will be available trough 'context'
+ *                   pointer in the routine code.
+ */
+#define UCOROUTINE_FUNC_BEGIN(__name, __dataType) __UCOROUTINE_FUNC_BEGIN(__name, __dataType)
+#define UCOROUTINE_FUNC_END                       __UCOROUTINE_FUNC_END
+
+/*!
+ * Initializes internals.
+ */
 void uCoroutine_initialize(void);
 
+/*!
+ * Terminates internals.
+ */
 void uCoroutine_terminate(void);
 
 #ifdef UCOROUTINE_CONFIG_DYNAMIC_ALLOCATION
+/*!
+ * Allocates a new uCorotine object and configures it by calling
+ * uCoroutine_configure() internally.
+ *
+ * @param priority coroutine priority
+ * @param func     coroutine function
+ * @param funcData user data provided to coroutine function
+ */
 uCoroutinePtr uCoroutine_new(
 	uCoroutinePriority priority,
 	uCoroutineFunc     func,
@@ -30,6 +60,15 @@ uCoroutinePtr uCoroutine_new(
 );
 #endif
 
+/*!
+ * Prepares/configures uCoroutine object. This method should be called
+ * before starting coroutine. Calling this method does not automatically start
+ * the coroutine.
+ *
+ * @param priority coroutine priority
+ * @param func     coroutine function
+ * @param funcData user data provided to coroutine function
+ */
 void uCoroutine_configure(
 	uCoroutinePtr      coroutine,
 	uCoroutinePriority priority,
@@ -37,16 +76,40 @@ void uCoroutine_configure(
 	void              *funcData
 );
 
+/*!
+ *
+ */
 void uCoroutine_start(uCoroutinePtr coroutine);
 
+/*!
+ *
+ */
 void uCoroutine_stop(uCoroutinePtr coroutine);
 
+/*!
+ *
+ */
 #define uCoroutine_yield                uCoroutine_internal_yield
 
+/*!
+ *
+ */
 #define uCoroutine_sleep(seconds)       uCoroutine_internal_sleep(seconds)
+
+/*!
+ *
+ */
 #define uCoroutine_sleepMs(miliseconds) uCoroutine_internal_sleepMs(miliseconds)
+
+/*!
+ *
+ */
 #define uCoroutine_sleepTicks(ticks)    uCoroutine_internal_sleepTicks(ticks)
 
+/*!
+ * Coroutines scheduler mainloop. This method never returns unless
+ * uCoroutine_interrupt() function is called.
+ */
 void uCoroutine_schedule(void);
 
 /*!
@@ -54,6 +117,9 @@ void uCoroutine_schedule(void);
  */
 void uCoroutine_interrupt(void);
 
+/*!
+ * This function is called when no coroutine can run in a loop run.
+ */
 void uCoroutine_idle_callback(void) __attribute__((weak));
 
 #ifdef __cplusplus
--- include/uCoroutine/internal.h
+++ include/uCoroutine/internal.h
@@ -16,4 +16,22 @@ struct _uCoroutine {
 	uint16_t pc;
 };
 
+
+#define __UCOROUTINE_BEGIN(_ucPtr) \
+	switch (((uCoroutine *)_ucPtr)->pc) { \
+		case UCOROUTINE_PC_INIT:
+
+#define __UCOROUTINE_END() \
+		default: \
+			break; \
+	}
+
+#define __UCOROUTINE_FUNC_BEGIN(__name, __dataType) static void __name(uCoroutine *_self, void *_taskData) { \
+	__dataType *context = (__dataType *) __taskData; \
+	\
+	__UCOROUTINE_BEGIN(_self);
+
+#define __UCOROUTINE_FUNC_END \
+	__UCOROUTINE_END();
+
 #endif /* UCOROUTINE_INTERNAL_H_ */
